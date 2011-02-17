1c9be5501e7b461ab76ff40c86a76736e68417f9
Sokolov Yura <funny.falcon@gmail.com>
2011-02-17 09:28:41 +0300

    fix gist contained

--- prefix.c
+++ prefix.c
@@ -1025,7 +1025,11 @@ bool pr_consistent(StrategyNumber strategy,
       return pr_contains(key, query, true);
 
     case 2:
-      return pr_contains(query, key, true);
+      if ( is_leaf ) {
+        return pr_contains(query, key, true);
+      } else {
+	return pr_overlaps(query, key);
+      }
 
     case 3:
       if( is_leaf ) {
